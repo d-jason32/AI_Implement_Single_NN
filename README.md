7dc375340db38362a96f795bb2fd8044a38e7d76
Jason Devaraj <jdevaraj01@gmail.com>
2025-10-30 15:44:55 -0400

    Finish.

---
+++ README.md
@@ -0,0 +1,8 @@
+# AI Module 5 Assignment Single Neural Network
+
+### Documentation
+
+1. Edited the code in the powerpoint for a Single Neural Network. 
+2. Training data edited for y = 3x + 2.3
+3. 20 different data units were added that follow the provided equation.
+4. Epoch ran for 100,000 iterations
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import sys
-def main():
-    x0 = [1, 2,3,4,5,6,7,8,9,10]
-    x1 = [2.2, 4.5, 6.1 , 11.2, 10.6, 12.2, 13.8, 16.0, 18.7, 21 ]
-    y = [0, 1, 0, 1, 1, 0, 0, 0, 1, 1]
 
+'''
+Using the code provided in the PPT for Single NN. 
+Implement the solution for the following equation
+
+y= 3x+2.3
+
+Train the algorithm with 20 data units and run the epoch for 100000 iterations.
+
+Please provide your documentation for the implementation 
+'''
+
+
+
+def main():
+    x0 = [1, 2,3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    x1 = [5.2, 8.4, 11.2 , 14.4, 17.2, 20.4, 23.2, 26.4, 29.2, 32.4, 35.2, 38.4, 41.2, 44.4, 47.4, 50.2, 53.4, 56.2, 59.3, 62.2 ]
+    # Expected outputs
+    y = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1,0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+    # Initialize weights and biases
     w0 = 0.1
     w1 = -0.23
     b = 0.22
 
+    # Epoch running for 100,000 iterations
     for i in range(0, 100000):
         loss = 0
         for j in range(0, len(y)):
@@ -26,7 +43,7 @@ def main():
         print("loss = ", loss)
 
     x0_test = 2.7
-    x1_test = 5.4
+    x1_test = 6.0
     output = x0_test * w0 + x1_test * w1 + b
     print("Output ", output)
 
